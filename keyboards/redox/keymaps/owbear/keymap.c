411033d13c5295ab0753bac76a4c17a584f3773e
Bjorn Eriksson <bjorn.eriksson@consilium.se>
2021-01-20 13:56:10 +0100

    Add a SE layer
    * Toggle using AltGr on Layer 1

--- keyboards/redox/keymaps/owbear/keymap.c
+++ keyboards/redox/keymaps/owbear/keymap.c
@@ -14,6 +14,7 @@
 enum layers
 {
     _US,  // Default ASCII
+    _SE,  // ASCII with SE
     _SYM, // Symbols
     _NAV, // Navigational
     _SYS  // Random stuff
@@ -35,24 +36,32 @@ KC_LSFT,          KC_Z,     KC_X,          KC_C,             KC_V,          KC_B
 LCTL_T(KC_ESC),   KC_LCTL,  KC_LEFT,       LALT_T(KC_RGHT),  vvvvv          TT(_SYM),  KC_BSPC,   KC_DEL,   vvvvv   KC_ENT,   KC_SPC,            MO(_SYM),  vvvvv          LALT_T(KC_DOWN),  KC_UP,    KC_RCTL,  RCTL_T(KC_ENT)
 )
 ,
+[_SE]             =         LAYOUT(
+_______,          _______,  _______,       _______,          _______,       _______,   vvvvv      vvvvv     vvvvv   vvvvv     vvvvv              _______,   _______,       _______,          _______,  _______,     _______,
+_______,          _______,  _______,       _______,          _______,       _______,   _______,   vvvvv     vvvvv   vvvvv     _______,           _______,   _______,       _______,          _______,  _______,     RALT(KC_W),
+_______,          _______,  _______,       _______,          _______,       _______,   _______,   vvvvv     vvvvv   vvvvv     _______,           _______,   _______,       _______,          _______,  RALT(KC_P),  RALT(KC_Q), // Would like RGUI_T(RALT(KC_Q))
+_______,          _______,  _______,       _______,          _______,       _______,   _______,   _______,  vvvvv   _______,  _______,           _______,   _______,       _______,          _______,  _______,     _______,
+_______,          _______,  _______,       _______,          vvvvv          _______,   _______,   _______,  vvvvv   _______,  _______,           _______,   vvvvv          _______,          _______,  _______,     _______
+)
+,
 [_SYM]            =         LAYOUT(
-MO(_SYS),         _______,  _______,       _______,          _______,       _______,   vvvvv      vvvvv     vvvvv   vvvvv     vvvvv              _______,   _______,       _______,          KC_F11,   KC_F12,   MO(_SYS),
-_______,          KC_EXLM,  KC_AT,         KC_LBRC,          KC_RBRC,       KC_PIPE,   _______,   vvvvv     vvvvv   vvvvv     _______,           KC_PSLS,   KC_7,          KC_8,             KC_9,     KC_PAST,  _______,
-_______,          KC_HASH,  KC_DLR,        KC_LPRN,          KC_RPRN,       KC_AMPR,   _______,   vvvvv     vvvvv   vvvvv     _______,           KC_PMNS,   KC_4,          KC_5,             KC_6,     KC_PPLS,  KC_NO,
-_______,          KC_PERC,  KC_CIRC,       KC_LCBR,          KC_RCBR,       KC_TILD,   _______,   _______,  vvvvv   _______,  _______,           KC_0,      KC_1,          KC_2,             KC_3,     KC_PENT,  KC_NO,
-_______,          _______,  _______,       _______,          vvvvv          _______,   _______,   _______,  vvvvv   _______,  _______,           KC_0,      vvvvv          _______,          KC_PDOT,  KC_PENT,  KC_APP
+MO(_SYS),         _______,  _______,       _______,          _______,       _______,   vvvvv      vvvvv     vvvvv   vvvvv     vvvvv              _______,   _______,       _______,          KC_F11,   KC_F12,      MO(_SYS),
+_______,          KC_EXLM,  KC_AT,         KC_LBRC,          KC_RBRC,       KC_PIPE,   _______,   vvvvv     vvvvv   vvvvv     _______,           KC_PSLS,   KC_7,          KC_8,             KC_9,     KC_PAST,     _______,
+_______,          KC_HASH,  KC_DLR,        KC_LPRN,          KC_RPRN,       KC_AMPR,   _______,   vvvvv     vvvvv   vvvvv     _______,           KC_PMNS,   KC_4,          KC_5,             KC_6,     KC_PPLS,     KC_NO,
+_______,          KC_PERC,  KC_CIRC,       KC_LCBR,          KC_RCBR,       KC_TILD,   TG(_SE),   _______,  vvvvv   _______,  _______,           KC_0,      KC_1,          KC_2,             KC_3,     KC_PENT,     KC_NO,
+_______,          _______,  _______,       _______,          vvvvv          _______,   _______,   _______,  vvvvv   _______,  _______,           KC_0,      vvvvv          _______,          KC_PDOT,  KC_PENT,     KC_APP
 )
 ,
 [_NAV]            =         LAYOUT(
-_______,          _______,  _______,       _______,          _______,       _______,   vvvvv      vvvvv     vvvvv   vvvvv     vvvvv              _______,   _______,       _______,          _______,  _______,  _______,
-KC_NO,            RESET,    KC_MS_U,       KC_NO,            KC_WH_U,       KC_NO,     _______,   vvvvv     vvvvv   vvvvv     _______,           KC_NO,     KC_NO,         KC_NO,            KC_NO,    KC_NO,    KC_NO,
-KC_NO,            KC_MS_L,  KC_MS_D,       KC_MS_R,          KC_WH_D,       KC_NO,     _______,   vvvvv     vvvvv   vvvvv     _______,           KC_LEFT,   KC_DOWN,       KC_UP,            KC_RGHT,  KC_NO,    KC_NO,
-KC_NO,            KC_NO,    LSFT(KC_DEL),  LCTL(KC_INS),     LSFT(KC_INS),  KC_NO,     _______,   _______,  vvvvv   _______,  KC_NO,             KC_NO,     KC_NO,         KC_NO,            KC_NO,    KC_NO,    KC_NO,
-KC_NO,            KC_NO,    KC_NO,         KC_NO,            vvvvv          KC_BTN1,   KC_BTN2,   _______,  vvvvv   _______,  _______,           KC_NO,     vvvvv          KC_NO,            KC_NO,    KC_NO,    KC_NO
+_______,          _______,  _______,       _______,          _______,       _______,   vvvvv      vvvvv     vvvvv   vvvvv     vvvvv              _______,   _______,       _______,          _______,  _______,     _______,
+KC_NO,            RESET,    KC_MS_U,       KC_NO,            KC_WH_U,       KC_NO,     _______,   vvvvv     vvvvv   vvvvv     _______,           KC_NO,     KC_NO,         KC_NO,            KC_NO,    KC_NO,       KC_NO,
+KC_NO,            KC_MS_L,  KC_MS_D,       KC_MS_R,          KC_WH_D,       KC_NO,     _______,   vvvvv     vvvvv   vvvvv     _______,           KC_LEFT,   KC_DOWN,       KC_UP,            KC_RGHT,  KC_NO,       KC_NO,
+KC_NO,            KC_NO,    LSFT(KC_DEL),  LCTL(KC_INS),     LSFT(KC_INS),  KC_NO,     _______,   _______,  vvvvv   _______,  KC_NO,             KC_NO,     KC_NO,         KC_NO,            KC_NO,    KC_NO,       KC_NO,
+KC_NO,            KC_NO,    KC_NO,         KC_NO,            vvvvv          KC_BTN1,   KC_BTN2,   _______,  vvvvv   _______,  _______,           KC_NO,     vvvvv          KC_NO,            KC_NO,    KC_NO,       KC_NO
 )
 ,
 [_SYS]            =         LAYOUT(
-KC_NO,            KC_NO,    KC_NO,         KC_NO,            KC_NO,         KC_NO,     KC_NO,     KC_NO,    KC_NO,  KC_NO,    KC_NO,             KC_NO,     KC_NO,         RESET,            RGB_M_P,  RGB_TOG,  RGB_MOD,          RGB_HUD,  KC_NO,  KC_NO,  RGB_SAI,  RGB_VAD,  RGB_VAI,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_TRNS,  KC_NO,  KC_NO,  KC_TRNS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO
+KC_NO,            KC_NO,    KC_NO,         KC_NO,            KC_NO,         KC_NO,     KC_NO,     KC_NO,    KC_NO,  KC_NO,    KC_NO,             KC_NO,     KC_NO,         RESET,            RGB_M_P,  RGB_TOG,     RGB_MOD,             RGB_HUD,  KC_NO,  KC_NO,  RGB_SAI,  RGB_VAD,  RGB_VAI,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_TRNS,  KC_NO,  KC_NO,  KC_TRNS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO
 )
 };
 /*END*/
