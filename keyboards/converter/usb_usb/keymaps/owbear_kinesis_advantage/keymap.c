f2b29c41b78a0c45a347bd4ed8e8b0d7030ecaee
Bjorn Eriksson <bjorn.eriksson@consilium.se>
2021-04-01 22:46:12 +0200

    Left win on CAPS

--- keyboards/converter/usb_usb/keymaps/owbear_kinesis_advantage/keymap.c
+++ keyboards/converter/usb_usb/keymaps/owbear_kinesis_advantage/keymap.c
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,        KC_F4,          KC_F5,  KC_F6,  KC_F7,        KC_F8,         KC_F9,   KC_F10,   KC_F11,          KC_F12,                                                  KC_PSCR, KC_SLCK, KC_PAUS,
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,        KC_F4,          KC_F5,  KC_F6,  KC_F7,        KC_F8,         KC_F9,   KC_F10,   KC_MINS,         KC_EQL,    KC_BSPC,       KC_INS,  RaltHome, KC_PGUP,    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
     KC_TAB,  KC_Q,    KC_W,    KC_E,         KC_R,           KC_T,   KC_Y,   KC_U,         KC_I,          KC_O,    KC_P,     KC_LBRC,         KC_RBRC,   KC_BSLS,       KC_DEL,  KC_END,  KC_PGDN,     KC_P7,   KC_P8,   KC_P9,
-    KC_CAPS, KC_A,    KC_S,    LALT_T(KC_D), LCTL_T(KC_F),   KC_G,   KC_H,   RCTL_T(KC_J), LALT_T(KC_K),  KC_L,    KC_SCLN,  RGUI_T(KC_QUOT), KC_ENT,                                                  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+    KC_LGUI, KC_A,    KC_S,    LALT_T(KC_D), LCTL_T(KC_F),   KC_G,   KC_H,   RCTL_T(KC_J), LALT_T(KC_K),  KC_L,    KC_SCLN,  RGUI_T(KC_QUOT), KC_ENT,                                                  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
     KC_LSFT, KC_Z,    KC_X,    KC_C,         KC_V,           KC_B,   KC_N,   KC_M,         KC_COMM,       KC_DOT,  KC_SLSH,  RSFT_T(KC_BSLS),                                   KC_DOWN,               KC_P1,   KC_P2,   KC_P3,
     TT(_SYM), KC_LGUI, KC_LALT,                      KC_SPC,                                               KC_RALT, KC_RGUI, KC_APP,          TT(_SYM),                KC_LEFT, KC_UP, KC_RGHT,        KC_P0,   KC_PDOT, KC_PENT
     ),
