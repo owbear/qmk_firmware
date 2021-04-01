d96845e1c30d5d2903e3322115d9af13e74de22c
Bjorn Eriksson <bjorn.eriksson@consilium.se>
2021-04-01 18:44:42 +0200

    .

--- keyboards/redox/keymaps/owbear/keymap.c
+++ keyboards/redox/keymaps/owbear/keymap.c
@@ -27,10 +27,13 @@ enum layers
 // Plans/ideas:
 // - Remove LALT_T arrow as pressing and holding arrows to scroll seems natural and I never use those Alt
 // - Add åäö on ['; on _SYM layer as AltGr on home is a bit difficult to reach
-// -- Collorary, make [': available on _SE layer?
-// - _SYM[KC_LSFT] for caps lock
-// -- Or go with KC_LOCK?
-// -- This means I shoud stop producing '%' with shift+mod+K and problably move % to a better place
+// ** Collorary, make [': available on _SE layer?
+// - _SYM[KC_LGUI] for caps lock
+// ** Or go with KC_LOCK?
+// * Move '%' % to a better place like _SYM[T] or _SYM[B]?
+// * Add backslash on KC_LSFT too?
+// * Make KC_ESC more accessible
+//
 // - Hold XCV for cut,copy and paste?
 //
 // Formatted using :?\[_US\]?,/\<END\>/!column -t
