6469229bcb21bad54d27577d6bcbd7d6fb1af861
Bjorn Eriksson <bjorn.eriksson@consilium.se>
2021-04-01 22:44:06 +0200

    More mappings from my Redox config
    * Upper row as Fx
    * Ctrl pressed+
    ** Left side as symbols
    ** Right side as numpad
    ** åäö available as expexted on Swedish keyboard

--- keyboards/converter/usb_usb/keymaps/owbear_kinesis_advantage/keymap.c
+++ keyboards/converter/usb_usb/keymaps/owbear_kinesis_advantage/keymap.c
@@ -21,14 +21,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum layers
 {
     _US,  // Default ASCII
-    _SE,  // ASCII with SE
+    //_SE,  // ASCII with SE
     _SYM, // Symbols
     _NAV, // Navigational
     _SYS, // Random stuff
     _PAD  // Not used
 };
 
+#define ____ KC_TRNS
+#define _____ KC_TRNS
+#define ______ KC_TRNS
 #define _______ KC_TRNS
+#define ________ KC_TRNS
+#define _________ KC_TRNS
+#define __________ KC_TRNS
+#define ____________ KC_TRNS
 #define vvvvv
 #define RaltHome RALT_T(KC_HOME)
 
@@ -84,22 +91,21 @@ enum layers
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_US] = LAYOUT_ansi(
-    KC_ESC,           KC_F1,   KC_F2,        KC_F3,          KC_F4,  KC_F5,  KC_F6,        KC_F7,         KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,        KC_PSCR, KC_SLCK, KC_PAUS,
-    KC_GRV,  KC_1,    KC_2,    KC_3,         KC_4,           KC_5,   KC_6,   KC_7,         KC_8,          KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,       KC_INS,  RaltHome, KC_PGUP,     KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,         KC_R,           KC_T,   KC_Y,   KC_U,         KC_I,          KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,       KC_DEL,  KC_END,  KC_PGDN,      KC_P7,   KC_P8,   KC_P9,
-    KC_CAPS, KC_A,    KC_S,    LALT_T(KC_D), LCTL_T(KC_F),   KC_G,   KC_H,   RCTL_T(KC_J), LALT_T(KC_K),  KC_L,    KC_SCLN, RGUI_T(KC_QUOT),    KC_ENT,                                        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-    KC_LSFT,          KC_Z,    KC_X,         KC_C,           KC_V,   KC_B,   KC_N,         KC_M,          KC_COMM, KC_DOT,  KC_SLSH,            RSFT_T(KC_BSLS),        KC_DOWN,               KC_P1,   KC_P2,   KC_P3,
-    KC_LCTL, KC_LGUI, KC_LALT,                      KC_SPC,                                               KC_RALT, KC_RGUI, KC_APP,    KC_RCTL,                KC_LEFT, KC_UP, KC_RGHT,        KC_P0,   KC_PDOT, KC_PENT
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,        KC_F4,          KC_F5,  KC_F6,  KC_F7,        KC_F8,         KC_F9,   KC_F10,   KC_F11,          KC_F12,                                                  KC_PSCR, KC_SLCK, KC_PAUS,
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,        KC_F4,          KC_F5,  KC_F6,  KC_F7,        KC_F8,         KC_F9,   KC_F10,   KC_MINS,         KC_EQL,    KC_BSPC,       KC_INS,  RaltHome, KC_PGUP,    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,         KC_R,           KC_T,   KC_Y,   KC_U,         KC_I,          KC_O,    KC_P,     KC_LBRC,         KC_RBRC,   KC_BSLS,       KC_DEL,  KC_END,  KC_PGDN,     KC_P7,   KC_P8,   KC_P9,
+    KC_CAPS, KC_A,    KC_S,    LALT_T(KC_D), LCTL_T(KC_F),   KC_G,   KC_H,   RCTL_T(KC_J), LALT_T(KC_K),  KC_L,    KC_SCLN,  RGUI_T(KC_QUOT), KC_ENT,                                                  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,         KC_V,           KC_B,   KC_N,   KC_M,         KC_COMM,       KC_DOT,  KC_SLSH,  RSFT_T(KC_BSLS),                                   KC_DOWN,               KC_P1,   KC_P2,   KC_P3,
+    TT(_SYM), KC_LGUI, KC_LALT,                      KC_SPC,                                               KC_RALT, KC_RGUI, KC_APP,          TT(_SYM),                KC_LEFT, KC_UP, KC_RGHT,        KC_P0,   KC_PDOT, KC_PENT
     ),
 
-    [1] = LAYOUT_all(
-                      ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______, ______,
-    ______,           ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______, ______,             ______,______,______,    ______,______,______,______,    ______,
-    ______,  ______,  ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______, ______, ______,     ______,______,______,    ______,______,______,______,    ______,______,
-    ______,  ______,  ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______, ______,         ______,     ______,______,______,    ______,______,______,______,    ______,______,
-    ______,  ______,  ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______,         ______, ______,                              ______,______,______,______,    ______,______,
-    ______,  ______,  ______,  ______,  ______, ______, ______,  ______,  ______, ______,  ______, ______,         ______, ______,            ______,           ______,______,______,______,    ______,______,
-    ______,  ______,  ______,  ______,  ______,         ______,           ______, ______,  ______, ______, ______, ______, ______,     ______,______,______,    ______,       ______,______,    ______,______
+    [_SYM] = LAYOUT_ansi(
+    ______,  _____,   _____,   _____,        _____,          _____,    _____,  _____,        _____,         _____,   ______,   ______,          ______,                                                  _______, _______, ________,
+    ______,  _____,   _____,   _____,        _____,          _____,    _____,  _____,        _____,         _____,   ______,   _______,         ______,    _______,       ______,  ________, _______,    _______, _______, _______, ________,
+    ______,  KC_EXLM,  KC_AT,   KC_LBRC,     KC_RBRC,        KC_PIPE,  KC_PSLS,   KC_7,      KC_8,          KC_9,    KC_PAST,     ____,         ____,   RALT(KC_W),       ______,  ______,  _______,     _____,   _____,   ______,
+    _______, KC_HASH,  KC_DLR,    KC_LPRN,   KC_RPRN,        KC_AMPR,  KC_PMNS,   KC_4,      KC_5,          KC_6,    RALT(KC_P),  RALT(KC_Q),  ______,                                                  _____,   _____,   _____,   ________,
+    _______, KC_PERC,   KC_CIRC,   KC_LBRC,  KC_RCBR,        KC_TILD,  KC_PPLS,   KC_1,      KC_2,          KC_3,    _______,  ________,                                         _______,               _____,   _____,   ______,
+    ________, _______, _______,                      KC_0,                                                _______, _______, ______,          ________,                _______, _____, _______,        _____,   _______, _______
     )
 
 };
