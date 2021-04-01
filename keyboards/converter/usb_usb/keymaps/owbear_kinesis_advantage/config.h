a289537076a927ef97af4ce242f4ebf744302aac
Bjorn Eriksson <bjorn.eriksson@consilium.se>
2021-04-01 21:30:00 +0200

    Config tweaks

--- keyboards/converter/usb_usb/keymaps/owbear_kinesis_advantage/config.h
+++ keyboards/converter/usb_usb/keymaps/owbear_kinesis_advantage/config.h
@@ -1,3 +1,8 @@
 #pragma once
 
-// place overrides here
+#include "config_common.h"
+
+#define TAPPING_TERM 200
+/* #define PERMISSIVE_HOLD */
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TOGGLE 3
