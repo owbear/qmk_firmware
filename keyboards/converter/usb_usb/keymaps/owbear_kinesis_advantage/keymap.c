736ba7ea7d1e41658a29299aaf162fcfec0d1d40
Bjorn Eriksson <owbear@gmail.com>
2021-04-13 19:48:41 +0200

    Tap dance cut, copy and paste
    * My first tap dance experiment to get rid of KC_INS

--- keyboards/converter/usb_usb/keymaps/owbear_kinesis_advantage/keymap.c
+++ keyboards/converter/usb_usb/keymaps/owbear_kinesis_advantage/keymap.c
@@ -85,6 +85,18 @@ enum layers
     ),
 */
 
+// Tap Dance Declarations
+ enum {
+   TD_X_CUT = 0,
+   TD_C_COPY = 1,
+   TD_V_PASTE = 2,
+ };
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_X_CUT]   = ACTION_TAP_DANCE_DOUBLE(KC_X, LSFT(KC_DEL)),
+    [TD_C_COPY]  = ACTION_TAP_DANCE_DOUBLE(KC_C, LCTL(KC_INS)),
+    [TD_V_PASTE] = ACTION_TAP_DANCE_DOUBLE(KC_V, LSFT(KC_INS)),
+};
 
 // Build: make converter/usb_usb:owbear_kinesis_advantage && cp .build/converter_usb_usb_hasu_owbear_kinesis_advantage.hex /mnt/tmp/qmk/
 // Burn: timeout 3 && \bin\qmk_flash.bat converter_usb_usb_hasu_owbear_kinesis_advantage.hex
@@ -95,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,        KC_F4,          KC_F5,  KC_F6,  KC_F7,        KC_F8,         KC_F9,   KC_F10,   KC_NO,           KC_NO,     KC_BSPC,       KC_INS,  RaltHome, KC_PGUP,    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
     KC_TAB,  KC_Q,    KC_W,    KC_E,         KC_R,           KC_T,   KC_Y,   KC_U,         KC_I,          KC_O,    KC_P,     KC_LBRC,         KC_RBRC,   KC_NO,         KC_DEL,  KC_END,  KC_PGDN,     KC_P7,   KC_P8,   KC_P9,
     KC_LGUI, KC_A, LSFT_T(KC_S), LALT_T(KC_D), LCTL_T(KC_F),   KC_G,   KC_H,   RCTL_T(KC_J), LALT_T(KC_K), RSFT_T(KC_L), KC_SCLN,  RGUI_T(KC_QUOT), KC_ENT,                                                  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,         KC_V,           KC_B,   KC_N,   KC_M,         KC_COMM,       KC_DOT,  KC_SLSH,  RSFT_T(KC_BSLS),                                   KC_DOWN,               KC_P1,   KC_P2,   KC_P3,
+    KC_LSFT, KC_Z,  TD(TD_X_CUT), TD(TD_C_COPY), TD(TD_V_PASTE), KC_B, KC_N,   KC_M,         KC_COMM,       KC_DOT,  KC_SLSH,  RSFT_T(KC_BSLS),                                   KC_DOWN,               KC_P1,   KC_P2,   KC_P3,
     TT(_SYM), KC_LGUI, KC_LALT,                      KC_SPC,                                               KC_RALT, KC_RGUI, KC_APP,          TT(_SYM),                KC_LEFT, KC_UP, KC_RGHT,        KC_P0,   KC_PDOT, KC_PENT
     ),
 
