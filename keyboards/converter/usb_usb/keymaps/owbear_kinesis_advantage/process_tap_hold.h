4a096e5ac4943cf37ce75466ef1f0c0af127de81
Bjorn Eriksson <owbear@gmail.com>
2021-06-21 17:42:03 +0200

    Tap-hold reinstated
    * Updated recipe from
      https://gist.github.com/herlev/9f4a305b575ac58c0deda18438ede2de/revisions

--- keyboards/converter/usb_usb/keymaps/owbear_kinesis_advantage/process_tap_hold.h
+++ keyboards/converter/usb_usb/keymaps/owbear_kinesis_advantage/process_tap_hold.h
@@ -1,12 +1,12 @@
-// Copied from https://gist.github.com/herlev/9f4a305b575ac58c0deda18438ede2de
-#ifndef PROCESS_TAP_HOLD_H
-#define PROCESS_TAP_HOLD_H
+#pragma once
+#include "stdint.h"
 
 #define shiftActive() \
   (keyboard_report->mods & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT)))
 
 #define ACTION_TAP_HOLD_SHIFT(kc_tap, kc_tap_shift, kc_hold, kc_hold_shift) \
-  { kc_tap, kc_tap_shift, kc_hold, kc_hold_shift, 0, false }
+  { kc_tap, kc_tap_shift, kc_hold, kc_hold_shift, 0, th_default }
+
 #define ACTION_TAP_HOLD(kc_tap, kc_hold) \
   ACTION_TAP_HOLD_SHIFT(kc_tap, LSFT(kc_tap), kc_hold, LSFT(kc_hold))
 
@@ -14,46 +14,40 @@
   #define TAP_HOLD_DELAY 200
 #endif
 
-typedef struct tap_hold_action_t {
+typedef enum {
+  th_default,
+  th_first_press
+} tap_hold_state;
+
+typedef struct {
   uint16_t KC_tap;
   uint16_t KC_tap_shift;
   uint16_t KC_hold;
   uint16_t KC_hold_shift;
   uint16_t timer;
-  bool timer_active;
+  tap_hold_state state;
 } tap_hold_action_t;
 
 extern tap_hold_action_t tap_hold_actions[];
-
-// QK_TAP_HOLD and QK_TAP_HOLD_MAX should be defined in the enum
-// quantum_keycodes at qmk_firmware/quantum/quantum_keycodes.h
-extern uint16_t QK_TAP_HOLD;
-#define QK_TAP_HOLD_MAX QK_TAP_HOLD + 10
+const extern uint16_t QK_TAP_HOLD;
+const extern uint16_t QK_TAP_HOLD_SIZE;
+// const extern uint16_t QK_TAP_HOLD_MAX;
+// uint16_t QK_TAP_HOLD_MAX = sizeof(tap_hold_actions)/sizeof(tap_hold_actions[0]);
 
 void process_record_tap_hold(uint16_t keycode, keyrecord_t *record);
 void matrix_scan_tap_hold(void);
-
-//=============================================//
-// TODO: Put code under this line in a .c file //
-//=============================================//
-
-void selectAndSendKey(tap_hold_action_t *t, bool pressed);
+void selectAndSendKey(tap_hold_action_t *t, bool is_held);
 void tap(uint16_t keycode);
 
-static int8_t highest_th = -1;
-
 void matrix_scan_tap_hold(void) {
-  if (highest_th == -1) {
-    return;
-  }
-
-  for (uint8_t i = 0; i <= highest_th; i++) {
-    if (tap_hold_actions[i].timer_active &&
-        timer_elapsed(tap_hold_actions[i].timer) >= TAP_HOLD_DELAY) {
+  for (uint8_t i = 0; i <= QK_TAP_HOLD_SIZE-1; i++) {
+    if (tap_hold_actions[i].state == th_first_press && timer_elapsed(tap_hold_actions[i].timer) >= TAP_HOLD_DELAY) {
+      tap_hold_actions[i].state = th_default;
       selectAndSendKey(&tap_hold_actions[i], true);
     }
   }
 }
+
 void tap(uint16_t keycode) {
   if (keycode == KC_NO) {
     return;
@@ -83,6 +77,16 @@ void tap(uint16_t keycode) {
     mods |= MOD_BIT(KC_LALT);
   }
 
+  if (prev_mods & MOD_BIT(KC_RCTL)) {
+    mods |= MOD_BIT(KC_RCTL);
+  }
+  if (prev_mods & MOD_BIT(KC_LALT)) {
+    mods |= MOD_BIT(KC_LALT);
+  }
+  if (prev_mods & MOD_BIT(KC_RGUI)) {
+    mods |= MOD_BIT(KC_RGUI);
+  }
+
   set_mods(mods);
   send_keyboard_report();
 
@@ -92,30 +96,35 @@ void tap(uint16_t keycode) {
   set_mods(prev_mods);
   send_keyboard_report();
 }
-void selectAndSendKey(tap_hold_action_t *t, bool pressed) {
+
+void selectAndSendKey(tap_hold_action_t *t, bool is_held) {
   if (shiftActive()) {
-    tap(pressed ? t->KC_hold_shift : t->KC_tap_shift);
+    tap(is_held ? t->KC_hold_shift : t->KC_tap_shift);
   } else {
-    tap(pressed ? t->KC_hold : t->KC_tap);
+    tap(is_held ? t->KC_hold : t->KC_tap);
   }
-  t->timer_active = false;
 }
+
 void process_record_tap_hold(uint16_t keycode, keyrecord_t *record) {
-  if (QK_TAP_HOLD <= keycode && keycode <= QK_TAP_HOLD_MAX) {
+  if (QK_TAP_HOLD <= keycode && keycode <= QK_TAP_HOLD+QK_TAP_HOLD_SIZE-1) {
     uint16_t idx = keycode - QK_TAP_HOLD;
-    if ((int16_t)idx > highest_th) {
-      highest_th = idx;
-    }
     tap_hold_action_t *t = &tap_hold_actions[idx];
     if (record->event.pressed) {
       t->timer = timer_read();
-      t->timer_active = true;
+      t->state = th_first_press;
     } else {
-      if (timer_elapsed(t->timer) < TAP_HOLD_DELAY) {
+      if (t->state == th_first_press) {
+        t->state = th_default;
+        selectAndSendKey(t, false);
+      }
+    }
+  } else {
+    for (int i = 0; i < QK_TAP_HOLD_SIZE; ++i) {
+      tap_hold_action_t *t = &tap_hold_actions[i];
+      if (t->state == th_first_press) {
+        t->state = th_default;
         selectAndSendKey(t, false);
       }
     }
   }
 }
-
-#endif
